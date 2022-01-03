316fe82abeab6976ee0a47409809a1462af3d3a0
tpb <mark@bernoullinetworks.com>
2022-01-03 16:55:43 +0000

    add basic test for file and install steps

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-wget https://apt.puppet.com/puppet7-release-focal.deb
-sudo dpkg -i puppet7-release-focal.deb
+if [ ! -e puppet7-release-focal.deb ]
+then
+  wget https://apt.puppet.com/puppet7-release-focal.deb
+  sudo dpkg -i puppet7-release-focal.deb
+fi
+sudo apt-get update
+sudo apt-get install puppet-agent
